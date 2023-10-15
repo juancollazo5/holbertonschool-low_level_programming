7ac852e4c53ee6166b71cdac140aeb630d29cb4a
juancollazo5 <juancollazo577@outlook.com>
2023-10-15 23:36:11 +0000

    learning C

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main: Write a C program that prints
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}

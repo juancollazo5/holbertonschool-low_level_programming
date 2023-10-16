49ac483214357a3510a2900195e4d495428d5678
juancollazo5 <juancollazo577@outlook.com>
2023-10-16 22:40:27 +0000

    putchar

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+char a;
+
+if (a = 'a'; a <= 'z', a++)
+putchar(a);
+putchar('\n')
+return (0);
+}

7a59d1bae4e15747df9987d22d92bac29084739d
juancollazo5 <juancollazo577@outlook.com>
2023-10-16 20:56:00 +0000

    if else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  * Return: Always 0.
@@ -8,6 +10,9 @@ int main(void)
 {
 
 int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {

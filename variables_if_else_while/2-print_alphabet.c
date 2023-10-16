29dbdc3344c1644166eb85500ffdafd6ac9f4da0
juancollazo5 <juancollazo577@outlook.com>
2023-10-16 23:06:58 +0000

    putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * main - Entry point
@@ -8,8 +9,9 @@ int main(void)
 char c;
 
 for (c = 'a'; c <= 'z'; c++);
+
 putchar(c);
 putchar('\n');
 
 return (0);
-} 
+}

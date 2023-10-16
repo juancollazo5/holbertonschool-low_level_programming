1f3c942a777d9a5112bb3b08e7e4c1c370e97fa9
juancollazo5 <juancollazo577@outlook.com>
2023-10-16 22:47:21 +0000

    putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  * Return: Always 0.
@@ -8,7 +6,7 @@
 int main(void)
 {
  char a;
- if (a = 'a'; a <= 'z', a++)
+ for (a = 'a'; a <= 'z', a++)
  putchar(a);
  putchar('\n');
  return (0);

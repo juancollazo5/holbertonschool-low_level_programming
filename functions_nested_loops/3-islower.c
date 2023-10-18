ae1bbba963b8a03fde5e41484736138790a0f106
juancollazo5 <juancollazo577@outlook.com>
2023-10-18 23:36:23 +0000

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -7,16 +7,11 @@
  */
 int _islower(int c)
 {
-    int r;
+	if (c >= 'a' && c <= 'z');
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+	return (1);
+
+	else
 
 	return (0);
-	return (c >= 'a' && c <= 'z');
 }

3900334ac0e841690ca8ca5dc122e59de9b26860
juancollazo5 <juancollazo577@outlook.com>
2023-10-18 23:50:19 +0000

    sign number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - sign of number
+ *
+ * @n: variable
+ *
+ * Return: 1 or 0.
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

3f43678b01a674d23471c3396cae694b6c54c455
juancollazo5 <juancollazo577@outlook.com>
2023-10-22 21:53:21 +0000

    functions

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,2 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * _isupper - entry main
+ * @c: variale
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}

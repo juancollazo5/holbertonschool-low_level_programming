11e04c340ef649de79f0fe2d31b660279de6db8f
juancollazo5 <juancollazo577@outlook.com>
2023-10-22 23:00:56 +0000

    functions

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* print_numbers - entry name
-* Return: void
+*print_most_numbers - entry point
 */
 
-int print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
+	if (!(c == '2' || c == '4'))
 	_putchar(c);
 	}
 	_putchar('\n');

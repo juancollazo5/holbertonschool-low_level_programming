09b696b8552bd59d032ccb2523b346c3f387a676
juancollazo5 <juancollazo577@outlook.com>
2023-10-24 14:21:59 +0000

    pointers

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - function
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+        char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}

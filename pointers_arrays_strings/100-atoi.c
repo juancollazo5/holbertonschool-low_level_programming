a98fc4ccb89083067ef9ce2ed3ed4887d576cf5d
juancollazo5 <juancollazo577@outlook.com>
2023-10-25 23:02:19 +0000

    pointers

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _atoi - function
+ * @s: the string to convert
+ * Return: the converted string.
+ */
+int _atoi(char *s)
+
+int signo = 1
+int result = 0
+
+while (s[i] !='\0')
+{
+	i++;
+}
+	i--;
+for 
+

8eba88926c4c9b589ff13b146d67e343fc001890
juancollazo5 <juancollazo577@outlook.com>
2023-11-07 23:46:31 -0400

    malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,10 +2,9 @@
 #include <string.h>
 /**
  * *_strdup- create a pointer duplicate of a string
- * *
- * *@str: string to duplicate and point with malloc
- * *Return: ptr of array from malloc
- * */
+ * @str: string to duplicate and point with malloc
+ * Return: ptr of array from malloc
+ */
 char *_strdup(char *str)
 {
 		unsigned int size;

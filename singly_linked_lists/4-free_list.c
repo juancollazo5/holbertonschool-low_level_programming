8c76ffd47a6632a898d1f9deeceedb5943be530a
juancollazo5 <juancollazo577@outlook.com>
2023-12-04 00:09:46 +0000

    code change

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,46 +1,17 @@
 #include "lists.h"
 
 /**
- * add_node_end - adding new node at end of linked list.
- * @head: starting point of linked list.
- * @str: string of characters.
- *
- * Return: new node.
+ * free_list - free linked list.
+ * @head: pointer to linked list.
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
 
-	list_t *new_node, *tail;
-	/*Allocate new node.*/
-	new_node = malloc(sizeof(list_t));
-
-	if (str == NULL || new_node == NULL)	/*Error proof it.*/
-	return (NULL);
-	/*Copy string and measure lenght of new node.*/
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-
-	if (new_node->str == NULL)	/*Error proof it.*/
+	while (head != NULL)
 	{
-	free(new_node);
-	return (NULL);
+		free(head->str);
+		free(head);
+		head = head->next;
 	}
-	/*new node will be the last node so, the pointer goes NULL.*/
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-	*head = new_node;
-	return (new_node);
-	}
-
-	tail = *head;
-
-	while (tail->next != NULL)
-	tail = tail->next;
-
-	/*Update the pointer to the last node.*/
-	tail->next = new_node;
-return (new_node);
 }
